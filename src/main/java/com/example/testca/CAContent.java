d53d7316ae79d4c381fe2711a8631d4b10ef15f5
johnsonfu <johnsonfu@hk110200065.convoy.local>
2023-01-30 15:29:04 +0800

    init

---
+++ src/main/java/com/example/testca/CAContent.java
@@ -0,0 +1,31 @@
+package com.example.testca;
+
+public class CAContent {
+	
+	private String lastName;
+	private String firstName;
+	private String binaryData;
+	
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getBinaryData() {
+		return binaryData;
+	}
+	public void setBinaryData(String binaryData) {
+		this.binaryData = binaryData;
+	}
+	
+	
+	
+
+}
---
+++ src/test/java/com/example/testca/TestCaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestCaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
